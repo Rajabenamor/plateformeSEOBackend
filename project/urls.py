dbdb65eeddf6cb3018cbee74c9d033ee4c164fcb
Raja <rajabenamor538@gmail.com>
2026-05-11 21:37:19 +0100

    fixed email sending with brevo template and added swagger

--- project/urls.py
+++ project/urls.py
@@ -15,13 +15,25 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path , include
-
+from django.urls import path , include ,  re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+# Configure the Swagger view
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Strive API",
+      default_version='v1',
+      description="Documentation for the Strive Authentication API",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
-    
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('api/auth/', include('authentication.urls')),
     path('api/admin/', include('authentication.admin_urls')),
     path('api/analysis/', include('analysis.urls')),
